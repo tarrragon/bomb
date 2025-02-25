c400a1c87218af6daa55a3ba32e14c1bd2b849b1
Tar <tarragonstop@gmail.com>
2025-02-26 05:24:11 +0800

    refactor: 重組測試目錄結構
    - 建立 test/providers/ 目錄
    - 移動 game_provider_test.dart 至對應目錄
    - 更新測試檔案的 import 路徑
    - 保持測試案例不變

--- lib/providers/game_provider.dart
+++ lib/providers/game_provider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'game_manager.dart';
+import '../core/game_manager.dart';
 
 /// 遊戲狀態提供者，負責將 GameManager 注入到 Widget 樹中
 class GameProvider extends ChangeNotifierProvider<GameManager> {
--- test/providers/game_provider_test.dart
+++ test/providers/game_provider_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
 import 'package:bomb/core/game_manager.dart';
-import 'package:bomb/core/game_provider.dart';
+import 'package:bomb/providers/game_provider.dart';
 
 void main() {
   testWidgets('GameProvider 應該正確提供 GameManager 實例', (
